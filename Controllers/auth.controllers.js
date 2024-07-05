7c95466d93d8cb58bdfa5f89bfe3716c14f0ff21
AbhiShukl18 <164164042+AbhiShukl18@users.noreply.github.com>
2024-07-05 21:52:45 +0530

    update

---
+++ Controllers/auth.controllers.js
@@ -0,0 +1,9 @@
+export const Login=(req,res)=>{
+
+    res.send("Login Successfull");
+}
+
+export const Register=(req,res)=>{
+
+    res.send("Registration successfull");
+}
--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-const express = require('express')
-const app = express()
+import express from 'express'
+import { Login, Register } from './Controllers/auth.controllers.js';
+const app = express();
+app.use(express.json()); //  parsing the json data into the javascript
 
-app.get('/', function (req, res) {
-  res.send('Hello Abhii ')
-})
+app.post("/", function (req, res) {
+ const {name, email, password }=req.body.userData;
+ if(name&&email&&password){
+
+  res.send("Data captured");
+ }
+ else{
+
+  res.send("All fields are mandatory");
+ }
+});
+
+app.post('/register', Register)
+app.post('/login', Login)
+
+app.get("/hello", function (req, res) {
+  res.send("Hello.");
+});
+
 
 app.listen(3000)
