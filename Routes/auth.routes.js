e874d5416f6ddd4f57d7cbd5aadb0e505670edde
AbhiShukl18 <164164042+AbhiShukl18@users.noreply.github.com>
2024-07-07 10:41:55 +0530

    update

---
+++ Routes/auth.routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { Login, Register } from "../Controllers/auth.controllers.js";
+
+const router = Router();
+
+router.post("/register", Register);
+router.post("/login", Login);
+
+export default router;
