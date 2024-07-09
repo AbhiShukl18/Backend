c0e338c998616b6b8dec2d7f07092673202bf1e2
AbhiShukl18 <164164042+AbhiShukl18@users.noreply.github.com>
2024-07-09 21:50:17 +0530

    updates

--- Controllers/auth.controllers.js
+++ Controllers/auth.controllers.js
@@ -1,5 +1,6 @@
 
 import User from "../Models/user.model.js";
+import bcrypt from "bcrypt";
 
 export const Login = (req, res) => {
   res.send("Login completed.");
@@ -12,15 +13,36 @@ export const Register = async (req, res) => {
       return res.json({ success: false, error: "All fields are required." });
     }
 
+    // checking email id is existing or not
+    const emailexist=await User.findOne({email:email});
+    console.log(emailexist,"emailexist");
+
+    if(emailexist){
+
+      return res.json(
+        {
+          
+          success:false,
+          error:"Email id already exist, try with different details"
+        }
+      )
+    }
+
+    const encryptedPassword= await bcrypt.hash(password,10);
+
+
     const newUser = new User({
       name: name,
       email: email,
-      password,
+      password:encryptedPassword,
     });
 
+    
     const responseFromDb = await newUser.save();
 
     return res.json({
+      encryptedPassword,
+      emailexist,
       success: true,
       responseFromDb,
       message: "Registeration Successfull.",
