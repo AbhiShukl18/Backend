6bf66eb4e5cf90f9d27515ae75621bad6a597d62
AbhiShukl18 <164164042+AbhiShukl18@users.noreply.github.com>
2024-07-19 15:32:06 +0530

    updates

--- Routes/admin.routes.js
+++ Routes/admin.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { LoginAdmin, RegisterAdmin  } from "../Controllers/admin.controllers.js";
+import { AdminLogin, RegisterAdmin, getCurrentAdmin, logoutAdmin  } from "../Controllers/admin.controllers.js";
 
 const router = Router();
 
-router.post("/login-admin", LoginAdmin);
+router.post("/login-admin", AdminLogin);
 router.post("/register-admin", RegisterAdmin);
+router.get('/get-current-admin', getCurrentAdmin)
+router.get('/logout', logoutAdmin)
+
 
 export default router;
