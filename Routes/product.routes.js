12c27f946507c693c27b0a0417c5628e0263bdfa
AbhiShukl18 <164164042+AbhiShukl18@users.noreply.github.com>
2024-08-04 22:26:02 +0530

    updates

--- Routes/product.routes.js
+++ Routes/product.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
   CreateNewProduct,
   filter,
+  agPipeline,
   GetAllProducts,
   GetSingleProducts,
 } from "../Controllers/product.controllers.js";
@@ -13,5 +14,7 @@ router.get("/get-all-product", GetAllProducts);
 router.post("/get-single-product", GetSingleProducts);
 router.post("/create-new-product", checkIsAdminValid, CreateNewProduct);
 router.post('/filter',filter)
+router.post('/aggreration-pipeline',agPipeline)
+
 
 export default router;
